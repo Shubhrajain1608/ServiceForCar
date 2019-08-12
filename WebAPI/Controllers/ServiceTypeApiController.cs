c693698ff54048395e89eba5602ab8cad228c807
Shubhrajain1608 <shubhra.jain1608@gmail.com>
2019-08-12 17:22:56 +0530

    commitdone

---
+++ WebAPI/Controllers/ServiceTypeApiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class ServiceTypeApiController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        // GET api/Employee
+    }
+}
